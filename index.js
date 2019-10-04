8ded32f98bcbf1f56089ee903075e424ca6fc331
Taran <thetaranneeld@gmail.com>
2019-10-03 18:34:18 -0600

    deployed api to heroku

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
+
 const server = require("./server");
 
-const port = 8000;
-server.listen(port, () => console.log('\nserver running\n'));
+const port = process.env.PORT;
+
+server.listen(port, () => console.log('\nserver running\n', port));
